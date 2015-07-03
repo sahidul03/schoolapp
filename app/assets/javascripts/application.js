6404a252d9bfa47ac27d0c8ac1d84ba5448b1b0c
sahidul03 <sahidul03@gmail.com>
2015-07-03 22:07:01 +0600

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,52 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require modernizr-2.6.2.min
+//= require marquee
+//= require jquery-migrate.min
+//= require jquery.validate.min
+//= require jquery-easing-1.3
+//= require jquery.sticky
+//= require jquery.nicescroll.min
+//= require jquery.inview
+//= require validation
+//= require jquery.tipTip.minified
+//= require jquery.bxslider.min
+//= require jquery.prettyPhoto
+//= require jquery.tweet.min
+//= require jquery.parallax-1.1.3
+//= require shortcodes
+//= require custom
+//= require jquery-transit-modified
+//= require layerslider.kreaturamedia.jquery
+//= require greensock
+//= require layerslider.transitions
+//= require jquery.themepunch.tools.min
+//= require jquery.themepunch.revolution.min
+//= require jquery.flexslider
+
+
+//= require_tree .
+//<script type="text/javascript">var lsjQuery = jQuery;</script>
+//<script type="text/javascript">
+//    var hh=lsjQuery(document).ready(function() {
+//        if(typeof lsjQuery.fn.layerSlider == "undefined") {
+//            lsShowNotice('layerslider_1','jquery');
+//        }
+//        else {
+//            lsjQuery("#layerslider_4").layerSlider({responsiveUnder: 1170, layersContainer: 1170, skinsPath: 'js/layerslider/skins/'});
+//        }
+//    });
+//    </script>
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+
+  def index
+    render layout: 'user_layout'
+  end
+
+end
