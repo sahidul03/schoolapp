4751710001c7d8469c753d6e13d1be940958697f
sahidul03 <sahidul03@gmail.com>
2015-08-09 20:21:35 +0600

    index page dynamic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, :except => [:index]
 
   def index
+    # raise Student.order("RAND()").first(2).inspect
     render layout: 'user_layout'
   end
 
